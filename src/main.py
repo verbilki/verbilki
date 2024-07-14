3f95e584cd0a266eba06d3e7a65e8b97b7448763
Oleg Zhadan <oleg.zhadan@yahoo.com>
2024-07-14 14:10:23 +0300

    Исправление ошибок в аннотациях типов и docstring функций filter_by_state и sort_by_date по результатам первого code review.

--- src/main.py
+++ src/main.py
@@ -19,15 +19,15 @@ from src.processing import filter_by_state, sort_by_date
 
 # Тестовый список словарей для проверки функций filter_by_state и sort_by_date
 test_data = [
-    {'id': 41428829,  'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
 
-print(filter_by_state(test_data, 'EXECUTED'))
+print(filter_by_state(test_data, "EXECUTED"))
 print(filter_by_state(test_data))
 
 print(sort_by_date(test_data))
-print(sort_by_date(test_data, sort_order=True))
-print(sort_by_date(test_data, sort_order=False))
+print(sort_by_date(test_data, True))
+print(sort_by_date(test_data, False))
--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,23 @@
-def filter_by_state(data: list, state: str = "EXECUTED") -> list:
+from typing import Any
+
+
+def filter_by_state(data: list[dict[str, Any]], state: str = "EXECUTED") -> list[dict[str, Any]]:
     """
-    Функция фильтрует список словарей data по состоянию state.
+    :Назначение функции: фильтрация списка словарей банковских операций по значению ключа 'state'.
+
+    :param data: список словарей банковских операций для фильтрации.
+    :param state: значение ключа 'state' для фильтрации. По умолчанию, 'EXECUTED'.
+    :return: список словарей.
     """
     return [item for item in data if item["state"] == state]
 
 
-def sort_by_date(data: list, sort_order: bool = True) -> list:
+def sort_by_date(data: list[dict[str, Any]], is_sort_order: bool = True) -> list[dict[str, Any]]:
     """
-    Функция сортирует список словарей data по атрибуту dqte
-    (по убыванию, если bool == True; по возрастанию, если bool == False).
+    :Назначение функции: сортировка списка словарей банковских операций по дате операции
+    :param data: список словарей банковских операций для фильтрации.
+    :param: is_sort_order: булевый флаг направления сортировки по датам операций.
+                          (True (по умолчанию) - по убыванию дат; False - по возрастанию дат).
+    :return: отсортированный список словарей
     """
-    return sorted(data, key=lambda item: item["date"], reverse=sort_order)
+    return sorted(data, key=lambda item: item["date"], reverse=is_sort_order)
