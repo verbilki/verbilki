809e7d7d29d761489c51adbeb5ca6adaaa66c45c
Oleg Zhadan <oleg.zhadan@yahoo.com>
2024-07-14 10:59:41 +0300

    Окончание Домашнего Задания 10-1 (ученик Олег Жадан).

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,13 @@
-def filter_by_state(data: list, state: str = 'EXECUTED') -> list:
-  """
-  Функция фильтрует список словарей data по состоянию state.
-  """
-  return [item for item in data if item['state'] == state]
+def filter_by_state(data: list, state: str = "EXECUTED") -> list:
+    """
+    Функция фильтрует список словарей data по состоянию state.
+    """
+    return [item for item in data if item["state"] == state]
+
 
 def sort_by_date(data: list, sort_order: bool = True) -> list:
-  """
-  Функция сортирует список словарей data по атрибуту dqte (по убыванию, если bool == True; по возрастанию, если bool == False).
-  """
-  return sorted(data, key=lambda item: item['date'], reverse=sort_order)
+    """
+    Функция сортирует список словарей data по атрибуту dqte
+    (по убыванию, если bool == True; по возрастанию, если bool == False).
+    """
+    return sorted(data, key=lambda item: item["date"], reverse=sort_order)
